71db56f2d4f02e8ca97784c67d2681d55f3eee3b
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-02-25 18:59:22 -0800

    hard section Q 12-13 completed

--- loops/Hard.js
+++ loops/Hard.js
@@ -1,2 +1,47 @@
 // ----------------------------------- 11 ------------------------------
 // armstrong number
+
+function armstrongChecker(number){
+    let total=0;
+    let originalNum=number;
+    let digitCount=number.toString().length;
+    while(number>0){
+        let lastNumber=Math.floor(number%10);
+        total+=(Math.pow(lastNumber,digitCount));
+        number=Math.floor(number/10);
+    };
+    if(total === originalNum){
+        return true;
+    }
+    else{
+        return false;
+    }; 
+};
+console.log(armstrongChecker(153));
+console.log(armstrongChecker(123));
+
+// ----------------------------------------------- 12 -------------------------------------------
+// Collatz Sequence (Hailstone Numbers)
+
+function Collatz(number){
+    let count=0;
+    while(number > 1){
+        if(number%2 === 0){
+            number=number/2;
+            count++;
+        }
+        else{
+            number=number*3+1;
+            count++;
+        };
+    };
+    return count;
+};
+console.log(Collatz(6));
+console.log(Collatz(10));
+
+// ------------------------------------- 13 --------------------------------------
+// convert decimal to binary
+
+
+
