c34d830186a8cfa100526d21ddae5d6605013e92
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-03-01 08:29:22 +0430

    hard section is remain

---
+++ functions/Hard.js
@@ -0,0 +1,77 @@
+// ------------------------------------- 11 ------------------------------
+//  Generate Pascal’s Triangle 
+// need eplanation please
+
+
+
+
+
+// ------------------------------------- 12 ------------------------------
+//  Binary Search Algorithm 
+// overall I know what is O(n) , O(log n) and how they work but for working the task I need your more explanations please
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function binarySearch(arr, target){
+//     let mid;
+//     // let mid=Math.floor(arr.length/2);
+//     let smallerArr=[];
+//     let biggerArr=[];
+//     if(target === arr[mid] ){
+//         return mid;
+//     }
+//     for(let i=0; i<arr.length; i++){
+//          mid=Math.floor(arr.length/2);
+//         if(arr[i]<mid){
+//             smallerArr.push(arr[i]); 
+//             arr= [...smallerArr]; 
+//         }
+//         else{
+//             biggerArr.push(arr[i]);
+//             arr=[...biggerArr];
+//         };
+         
+//     };
+
+//     console.log(smallerArr);
+//     console.log(biggerArr); 
+//     return -1;  
+// };
+// // binarySearch([1,2,3,4,5,6,7],4);
+// console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12],3));
+
+ 
