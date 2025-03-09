f2a6ab9d414bc71005a01e6038932689e69e226b
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2025-03-09 06:32:28 +0430

    hard section is remain

--- functions/Hard.js
+++ functions/Hard.js
@@ -1,24 +1,16 @@
 // ------------------------------------- 11 ------------------------------
 //  Generate Pascal’s Triangle 
-// need eplanation please
-
-
-
 
 
 // ------------------------------------- 12 ------------------------------
 //  Binary Search Algorithm 
-// overall I know what is O(n) , O(log n) and how they work but for working the task I need your more explanations please
-
-
-
 
 
 
 // ------------------------------------- 12 ------------------------------
 //  Convert Decimal to Binary 
 
-function decimalToBinary(n){
+// function decimalToBinary(n){
     // 00000000
     // 0000000 1
     // 000000 2 1
@@ -29,7 +21,11 @@ function decimalToBinary(n){
     // 0 64 32 16 8 4 2 1
     // 128 64 32 16 8 4 2 1
 
-}
+// }
+
+
+
+// I will do this section latter Insha'Allah.
 
 
 
